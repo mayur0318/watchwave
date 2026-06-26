20894d04fbb091b102ac252354ece719dc92d430
mayur0318 <mayurparmar0317@gmail.com>
2026-06-26 09:46:58 +0530

    utils code updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 // require("dotenv").config({ path: "./env" });
 import connectDb from "./db/dbConnection.js";
 
-connectDb();
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed !!");
+  });
